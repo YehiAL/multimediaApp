43587a54c302e72b5401e4be8061718072ab24b0
YehiAL <arayayehi01@gmail.com>
2024-10-16 16:12:50 -0300

    Avance clase 1

--- app/src/main/java/com/example/multimediaapp/MainActivity.java
+++ app/src/main/java/com/example/multimediaapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.multimediaapp;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button reproducirAudioButton = findViewById(R.id.playAudioButton);
+        reproducirAudioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.personaspecialistaudio);
+                mediaPlayer.start();
+            }
+        });
+
+        VideoView videoView = findViewById(R.id.videoView);
+        Button reproducirVideoButton = findViewById(R.id.playvideoButton);
+        reproducirVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/"+R.raw.personaspecialistvideo);
+                videoView.setVideoURI(videoUri);
+                videoView.start();
+            }
         });
     }
 }
