2875fb12e0f50b9da9b8a075d076d63cf8d0423c
YehiAL <arayayehi01@gmail.com>
2024-10-17 16:46:20 -0300

    Control de permisos

--- app/src/main/java/com/example/multimediaapp/MainActivity.java
+++ app/src/main/java/com/example/multimediaapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.multimediaapp;
 
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,21 +9,27 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.VideoView;
+import android.Manifest;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private static final int PERMISSION_REQUEST_CODE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermissions();
 
         Button reproducirAudioButton = findViewById(R.id.playAudioButton);
         reproducirAudioButton.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +60,27 @@ public class MainActivity extends AppCompatActivity {
                 imageView.startAnimation(fadeInAnimation);
             }
         });
+
+
+
+
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permiso aceptado", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
